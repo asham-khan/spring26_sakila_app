21f755925d666045821bda1d65cac94fd0e77207
asham-khan <ashamkhan2@gmail.com>
2026-04-26 17:06:19 +0500

    Successful results achieved in part B

--- config.py
+++ config.py
@@ -19,4 +19,3 @@ class Config:
     CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
     HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
-
