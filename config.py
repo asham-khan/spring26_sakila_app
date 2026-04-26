a5ffa947cd8390451bf4e53a9fde55d86e04f860
asham-khan <ashamkhan2@gmail.com>
2026-04-26 15:38:22 +0500

    Fixed missing Config issues in config.py

--- config.py
+++ config.py
@@ -10,3 +10,11 @@ import os
 MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
 CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
 HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+
+class Config:
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
+    CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+    HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
